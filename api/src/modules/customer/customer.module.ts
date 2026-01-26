5c3d2a886ba5090443d4a2897b4ec572cefc539b
sigwaldostepan <sigwaldos@gmail.com>
2026-01-26 13:47:56 +0700

    fix: remove typeorm entities on services that migrated to prisma

--- api/src/modules/customer/customer.module.ts
+++ api/src/modules/customer/customer.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CustomerService } from './customer.service';
-import { CustomerController } from './customer.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Customer } from './entities/customer.entity';
-import { PrismaModule } from 'src/infra/persistance/database/prisma/prisma.module';
 import { LoggerModule } from 'src/infra/logger/logger.module';
+import { PrismaModule } from 'src/infra/persistance/database/prisma/prisma.module';
+import { CustomerController } from './customer.controller';
+import { CustomerService } from './customer.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer]), PrismaModule, LoggerModule],
+  imports: [PrismaModule, LoggerModule],
   providers: [CustomerService],
   controllers: [CustomerController],
   exports: [CustomerService],
--- api/src/modules/item/item.module.ts
+++ api/src/modules/item/item.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { PrismaModule } from 'src/infra/persistance/database/prisma/prisma.module';
-import { Item } from './entities/item.entity';
 import { ItemController } from './item.controller';
 import { ItemService } from './item.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Item]), PrismaModule],
+  imports: [PrismaModule],
   controllers: [ItemController],
   providers: [ItemService],
 })
