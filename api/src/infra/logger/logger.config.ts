888042dae5ba3af91c28f5c4a893b85de1f4420c
sigwaldostepan <sigwaldos@gmail.com>
2026-05-04 16:41:41 +0700

    chore: remove unused file logging

--- api/src/infra/logger/logger.config.ts
+++ api/src/infra/logger/logger.config.ts
@@ -1,12 +1,10 @@
 import winston, { type LoggerOptions } from 'winston';
-import * as fs from 'fs';
-import * as path from 'path';
 
-const logDir = '/tmp/logs';
+// const logDir = '/tmp/logs';
 
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir, { recursive: true });
-}
+// if (!fs.existsSync(logDir)) {
+//   fs.mkdirSync(logDir, { recursive: true });
+// }
 
 export const loggerOptions: LoggerOptions = {
   format: winston.format.combine(
@@ -16,17 +14,17 @@ export const loggerOptions: LoggerOptions = {
   transports: [
     new winston.transports.Console(),
 
-    new winston.transports.File({
-      filename: path.join(logDir, 'error.log'),
-      level: 'error',
-      maxsize: 5 * 1024 * 1024,
-      maxFiles: 3,
-    }),
+    // new winston.transports.File({
+    //   filename: path.join(logDir, 'error.log'),
+    //   level: 'error',
+    //   maxsize: 5 * 1024 * 1024,
+    //   maxFiles: 3,
+    // } as any),
 
-    new winston.transports.File({
-      filename: path.join(logDir, 'combined.log'),
-      maxsize: 5 * 1024 * 1024,
-      maxFiles: 3,
-    }),
+    // new winston.transports.File({
+    //   filename: path.join(logDir, 'combined.log'),
+    //   maxsize: 5 * 1024 * 1024,
+    //   maxFiles: 3,
+    // }),
   ],
 };
