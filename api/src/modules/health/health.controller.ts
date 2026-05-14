c3d097b3e385605a04e096ae302aa44a6080cb73
sigwaldostepan <sigwaldos@gmail.com>
2026-05-14 17:24:01 +0700

    fix: deactivate auth guard for health check endpoint

--- api/src/modules/health/health.controller.ts
+++ api/src/modules/health/health.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { OptionalAuth } from '@thallesp/nestjs-better-auth';
 
 @Controller('health')
 export class HealthController {
+  @OptionalAuth()
   @Get()
   public check() {
     return {
